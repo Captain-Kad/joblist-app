29dc6bc827bd4dbd170d002fe941676736aac380
Captain_Kad <buckskad@gmail.com>
2025-05-05 21:54:59 -0400

    deleted some files and moved some

--- joblist/src/App.jsx
+++ joblist/src/App.jsx
@@ -1,9 +1,8 @@
-import "./App.css";
+import "./styles/App.css";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import Header from "./components/Header";
-
 import JobList from "./components/JobList";
 import Footer from "./components/Footer";
 
