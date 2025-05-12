0a568639787fb0d4a62be40587454290adb9f482
Captain_Kad <buckskad@gmail.com>
2025-05-12 11:37:19 -0400

    created the backend api for the job details and made the docker compose file such that it runs both the front end and back end simultaneously

--- joblist/src/App.jsx
+++ joblist/src/App.jsx
@@ -5,6 +5,15 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Header from "./components/Header";
 import JobList from "./components/JobList";
 import Footer from "./components/Footer";
+import JOBS_URL from "./constants"
+
+import axios from 'axios';
+
+axios.get(JOBS_URL).then(function (response) {
+  console.log(response);
+})
+
+
 
 
 function App() {
