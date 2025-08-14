6d3c8668f0984a07b5cbb4807aa676451bf904b3
Captain_Kad <buckskad@gmail.com>
2025-08-14 14:58:28 -0400

    created and env file to store secrets

--- node-testing/src/db.js
+++ node-testing/src/db.js
@@ -1,9 +1,4 @@
-// db.js - Sets up connection pool for PostgreSQL
 import pkg from 'pg';
 const { Pool } = pkg;
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
+const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export default pool;
